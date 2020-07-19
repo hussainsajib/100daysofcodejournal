878ad67b2e5dd499c93a465d9ac0b64a74b2616f
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-07-18 20:22:36 -0400

    fetching data from google sheet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
@@ -6,16 +6,24 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Layout from "./Layout/Layout";
 
-function App() {
-    return (
-        <div className="App">
-            <Layout>
-                <Switch>
-                    <Route path="/" component={Home} exact />
-                </Switch>
-            </Layout>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+                <Layout>
+                    <Switch>
+                        <Route path="/" component={Home} exact />
+                    </Switch>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
+import Tabletop from "tabletop";
+import { Card } from "react-bootstrap";
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-            someState: "",
+            data: [],
+            dataSize: 0,
         };
+    }
+
+    componentDidMount() {
+        Tabletop.init({
+            key: process.env.REACT_APP_GOOGLE_SHEET_KEY,
+            callback: (googleData) => {
+                this.setState({
+                    data: googleData,
+                    dataSize: googleData.length,
+                });
+            },
+            simpleSheet: true,
+        });
     }
 
     render() {
         return (
             <div>
-                <h1>Home</h1>
-                <p>This is the home page</p>
+                <h1>100 Days of Code</h1>
+                {this.state.dataSize === 0 ? (
+                    <p>There is not data available</p>
+                ) : (
+                    this.state.data.map((item) => (
+                        <Card style={{ width: "18rem" }}>
+                            <Card.Body>
+                                <Card.Title>Day {item.day}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">
+                                    {item.date}
+                                </Card.Subtitle>
+                                <Card.Text>{item.works}</Card.Text>
+                                <Card.Link href="#">Card Link</Card.Link>
+                                <Card.Link href="#">Another Link</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    ))
+                )}
             </div>
         );
     }
